f77cf75604f04edc3617f01496bb4d4d546fabe6
Renato Novaes <renatoalmeida49@gmail.com>
2021-05-02 20:09:21 -0300

    Primeira versão do formulário de cadastro de hospitais

--- src/views/Hospitais.vue
+++ src/views/Hospitais.vue
@@ -9,35 +9,51 @@
                     <h2>Adicionar hospital</h2>
 
                     <form>
-                        <label for="nome">Nome</label>
-                        <input v-model="hospital.nome" type="text" name="nome" id="nome" required />
+                        <div class="form-group fg1">
+                            <label for="nome">Nome</label>
+                            <input v-model="hospital.nome" type="text" name="nome" id="nome" required />
 
-                        <label for="cep">CEP</label>
-                        <input v-model="hospital.cep" type="text" name="cep" id="cep" />
+                            <label for="cep">CEP</label>
+                            <input v-model="hospital.cep" type="text" name="cep" id="cep" />
 
-                        <label for="endereco">Endereço</label>
-                        <input v-model="hospital.endereco" type="text" name="endereco" id="endereco" />
+                            <label for="endereco">Endereço</label>
+                            <input v-model="hospital.endereco" type="text" name="endereco" id="endereco" />
+                        </div>
+                        
+                        <div class="form-group fg2">
+                            <div>
+                                <label for="bairro">Bairro</label>
+                                <input v-model="hospital.bairro" type="text" name="bairro" id="bairro" />
+                            </div>
 
-                        <label for="bairro">Bairro</label>
-                        <input v-model="hospital.bairro" type="text" name="bairro" id="bairro" />
+                            <div>
+                                <label for="cidade">Cidade</label>
+                                <input v-model="hospital.cidade" type="text" name="cidade" id="cidade" />
+                            </div>
 
-                        <label for="cidade">Cidade</label>
-                        <input v-model="hospital.cidade" type="text" name="cidade" id="cidade" />
+                            <div>
+                                <label for="estado">Estado</label>
+                                <input v-model="hospital.estado" type="text" name="estado" id="estado" />
+                            </div>
+                        </div>
 
-                        <label for="estado">Estado</label>
-                        <input v-model="hospital.estado" type="text" name="estado" id="estado" />
+                        <div class="form-group fg2">
+                            <div>
+                                <label for="lat">Lat</label>
+                                <input v-model="hospital.lat" type="text" name="lat" id="lat" />
+                            </div>
 
-                        <label for="lat">Lat</label>
-                        <input v-model="hospital.lat" type="text" name="lat" id="lat" />
-
-                        <label for="long">Long</label>
-                        <input v-model="hospital.long" type="text" name="long" id="long" />
+                            <div>
+                                <label for="long">Long</label>
+                                <input v-model="hospital.long" type="text" name="long" id="long" />
+                            </div>
+                        </div>
 
                         <label for="info">Informações adicionais</label>
                         <textarea v-model="hospital.informacoes" name="info" id="info" rows="8"></textarea>
                         
-                        <div>
-                            <button @click="adicionar()">Adicionar</button>
+                        <div class="form-group">
+                            <button @click.prevent="adicionar()">Adicionar</button>
                             <button @click="modal = false">Cancelar</button>
                         </div>
                     </form>
@@ -60,6 +76,7 @@ export default {
         return {
             modal: false,
             hospital: {
+                id: '',
                 nome: '',
                 cep: '',
                 endereco: '',
@@ -78,21 +95,34 @@ export default {
                 this.modal = false
         },
         adicionar() {
-            fetch('https://getpantry.cloud/apiv1/pantry/e79d83be-93de-41de-8dff-60da1c35938f/basket/hospitais', {
+            this.hospital.id = Date.now()
+
+            //fetch('http://localhost:3000/hospitais', {
+            fetch(`https://getpantry.cloud/apiv1/pantry/e79d83be-93de-41de-8dff-60da1c35938f/basket/${this.hospital.id}`, {
                 method: 'POST',
-                mode: 'cors',
-                cache: 'no-cache',
-                credentials: 'same-origin',
+                body: JSON.stringify(this.hospital),
                 headers: {
-                    'Content-Type': 'application/json'
-                },
-                redirect: 'follow',
-                referrerPolicy: 'no-referrer',
-                body: this.hospital
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
             })
                 .then(data => {
                     console.log(data)
+                    this.reset()
                 })
+        },
+        reset() {
+            this.modal = false
+
+            this.hospital.id = ''
+            this.hospital.nome = ''
+            this.hospital.cep = ''
+            this.hospital.endereco = ''
+            this.hospital.bairro = ''
+            this.hospital.cidade = ''
+            this.hospital.estado = ''
+            this.hospital.lat = ''
+            this.hospital.long = ''
+            this.hospital.informacoes = ''
         }
     }
 }
@@ -141,7 +171,7 @@ button:hover {
     background: #fff;
     padding: 20px;
     margin: auto;
-    max-width: 320px;
+    max-width: max-content;
 }
 
 .modal form {
@@ -167,14 +197,32 @@ form textarea:hover {
 form input:focus,
 form textarea:focus {
     outline: none;
+    border-color: #39AAE1;
 }
 
 form label {
     margin-top: 1rem;
 }
 
-form div {
+.form-group {
     display: flex;
     justify-content: space-between;
+}
+
+.fg1 {
+    display: flex;
+    flex-direction: column;
+    margin: 8px 0;
+}
+
+.fg2 {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 8px 0;
+}
+
+.fg2 div {
+    display: flex;
+    flex-direction: column;
 }
 </style>
